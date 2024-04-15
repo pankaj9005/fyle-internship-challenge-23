38e4d05a417eddf60f41653c6c3e901172b70578
Pankaj Kumar Gupta <2001pankajkumargupta@gmail.com>
2024-04-15 19:19:47 +0530

    Complete the frontend assignment to changes in this repository.

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,19 +1,56 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
+import { Observable, throwError, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Repo } from '../Model/repo';
+import { User } from '../Model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private githubUrl = "https://api.github.com";
+  private repoCache: { [key: string]: Repo[] } = {};
 
-  constructor(
-    private httpClient: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
-  getUser(githubUsername: string) {
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
+  getUser(userName: string): Observable<any> {
+    return this.http.get<User>(`${this.githubUrl}/users/${userName}`).pipe(
+      catchError(this.handleError)
+    );
   }
 
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  getUserRepos(userName: string, page: number = 1, perPage: number = 10): Observable<Repo[]> {
+    const cacheKey = `${userName}-${page}-${perPage}`;
+
+    if (this.repoCache[cacheKey]) {
+      return of(this.repoCache[cacheKey]);
+    } else {
+      const apiURL = `${this.githubUrl}/users/${userName}/repos`;
+      const params = {
+        page: page.toString(),
+        per_page: perPage.toString()
+      };
+      const headers = new HttpHeaders().set('Accept', 'application/vnd.github.v3+json');
+      
+      return this.http.get<Repo[]>(apiURL, { params, headers }).pipe(
+        map((repos) => {
+          this.repoCache[cacheKey] = repos;
+          return repos;
+        }),
+        catchError(this.handleError)
+      );
+    }
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = `Error: ${error.status}`;
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Error: ${error.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
+  
+  
 }
